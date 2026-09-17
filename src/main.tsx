142930570257fa859bf6e7518a3b150533b687e9
Kumar Potla <kumarpotla2221@gmail.com>
2026-09-17 16:34:54 +0530

    final changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/new-rlklein">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
